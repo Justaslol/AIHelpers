80f3e24656b215658d4a0014e7331a7dac7e98a1
Justas Kondratavičius <>
2024-09-15 01:53:19 +0300

    Remove env values (((:

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,14 +1,22 @@
 import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
+import {
+  FIREBASE_API_KEY,
+  FIREBASE_AUTH_DOMAIN,
+  FIREBASE_PROJECT_ID,
+  FIREBASE_STORAGE_BUCKET,
+  FIREBASE_MESSAGING_SENDER_ID,
+  FIREBASE_APP_ID
+} from '@env';
 
 // Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyACR6ee2Tf7L1sn0ZkYTUBpCQ5UVCovvuM",
-    authDomain: "sintra-test-app.firebaseapp.com",
-    projectId: "sintra-test-app",
-    storageBucket: "sintra-test-app.appspot.com",
-    messagingSenderId: "410396289784",
-    appId: "1:410396289784:web:470efcb4832a312a38690d"
+    apiKey: FIREBASE_API_KEY,
+    authDomain: FIREBASE_AUTH_DOMAIN,
+    projectId: FIREBASE_PROJECT_ID,
+    storageBucket: FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: FIREBASE_MESSAGING_SENDER_ID,
+    appId: FIREBASE_APP_ID
   };
 
 // Initialize Firebase
